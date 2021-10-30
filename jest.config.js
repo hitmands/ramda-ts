a8d6e277d50535e05c60514209801d766e52a8f1
Giuseppe Mandato <giuseppemandato@gmail.com>
2021-10-30 16:31:21 +0200

    chore(jest): add configuration file

---
+++ jest.config.js
@@ -0,0 +1,8 @@
+/** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
+module.exports = {
+  rootDir: 'src',
+  collectCoverage: true,
+  coverageDirectory: '../coverage',
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+};
