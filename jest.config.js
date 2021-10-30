f8ab269d86dbaa0323203685e435782fc91ea62e
Giuseppe Mandato <giuseppemandato@gmail.com>
2021-10-30 12:23:23 +0200

    refactor(jest): collect coverage

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,8 @@
 /** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
 module.exports = {
+  rootDir: "src",
+  collectCoverage: true,
+  coverageDirectory: '../coverage',
   preset: 'ts-jest',
-  testEnvironment: 'node',
+  testEnvironment: 'node'
 };
